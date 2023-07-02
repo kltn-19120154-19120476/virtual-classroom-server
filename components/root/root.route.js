d0b64dbd5de5d0ce932391c1e17a421aec594ff5
nguyenminhuyen <nguyenminhuyen2001@gmail.com>
2023-07-02 15:20:52 +0700

    add document & recording

--- components/root/root.route.js
+++ components/root/root.route.js
@@ -1,11 +1,15 @@
 import authRoutes from "../auth/auth.routes.js";
 import roomRoutes from "../room/room.routes.js";
 import userRoutes from "../user/user.routes.js";
+import documentRouters from "../document/document.routes.js";
+import recordingRouters from "../recording/recording.routes.js";
 
 const route = (app) => {
   app.use("/api/auth", authRoutes);
   app.use("/api/room", roomRoutes);
   app.use("/api/user", userRoutes);
+  app.use("/api/document", documentRouters);
+  app.use("/api/recording", recordingRouters);
 };
 
 export default route;
