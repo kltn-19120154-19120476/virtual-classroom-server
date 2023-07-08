2a3e82cabe90c77a45a903cd9d805cc8f8bcc4a5
tranphuongdinh <dinh.tran@thuocsi.vn>
2023-07-08 11:51:55 +0700

    map document by userId and add API delete document

--- components/document/document.routes.js
+++ components/document/document.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import authenticationMiddleware from "../../middleware/auth.middleware.js";
-import { createDocument, getDocumentByPresIds } from "./document.controller.js";
+import {
+  createDocument,
+  deletePres,
+  getDocumentByPresIds,
+} from "./document.controller.js";
 const router = express.Router();
 
 router.post("/create", authenticationMiddleware, createDocument);
 router.post("/list", authenticationMiddleware, getDocumentByPresIds);
-
+router.delete("/delete", authenticationMiddleware, deletePres);
 export default router;
--- models/document.model.js
+++ models/document.model.js
@@ -1,13 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Document = new mongoose.Schema({
-//   podId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//   },
-  authorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
+  //   podId: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //   },
   presId: {
     type: String,
   },
@@ -17,18 +13,22 @@ const Document = new mongoose.Schema({
   uploadUrl: {
     type: String,
   },
-//   current: {
-//     type: mongoose.Schema.Types.ObjectId,
-//   },
-//   authzToken: {
-//     type: mongoose.Schema.Types.ObjectId,
-//   },
-//   uploadFailed: {
-//     type: mongoose.Schema.Types.ObjectId,
-//   },
-//   uploadFailReasons: {
-//     type: mongoose.Schema.Types.ObjectId,
-//   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  //   current: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //   },
+  //   authzToken: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //   },
+  //   uploadFailed: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //   },
+  //   uploadFailReasons: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //   },
 });
 
-export default mongoose.model('Document', Document);
+export default mongoose.model("Document", Document);
